1a5ee7d4b837cbfe8facad20187915e4e66e5a79
hpxro7 <2935981+hpxro7@users.noreply.github.com>
2018-10-18 01:54:57 -0700

    Wwise Vorbis headers are now parsed and printed.

--- hello.go
+++ hello.go
@@ -4,10 +4,14 @@ package main
 // #cgo LDFLAGS: -lvorbis -logg -lvorbisfile -lm
 // #include <stdlib.h>
 // #include "hello.h"
+// #include "streamfile.h"
+// #include "vgmstream.h"
+// #include "meta.h"
 import "C"
 
 import (
 	"fmt"
+	"unsafe"
 )
 
 func main() {
@@ -17,4 +21,12 @@ func main() {
 	n := C.say_hello((*C.char)(p))
 	b := C.GoBytes(p, n)
 	fmt.Println(string(b))
+
+	desc := (*C.char)(C.malloc(C.sizeof_char * 0x100))
+	defer C.free(unsafe.Pointer(desc))
+	f := C.open_stdio_streamfile(C.CString("test.wem"))
+	fmt.Printf("Streamfile: Type=%T, Value=%v\n\n", f, f)
+	s := C.init_vgmstream_wwise(f)
+	C.describe_vgmstream(s, (*C.char)(desc), 0x100)
+	fmt.Println("Descriptor: ", C.GoString(desc))
 }
